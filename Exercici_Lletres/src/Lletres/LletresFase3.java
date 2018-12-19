d48bd4257a5ec7fdf1a73d528ed7d6333bc8a534
Alejandro Calle Prado <alex_calle@hotmail.com>
2018-12-19 11:35:40 +0100

    Canvis a Fase 3: - Passo codi a anglés. - HashMap --> Map - ArrayList --> List - Canvis a funcions

--- Exercici_Lletres/src/Lletres/LletresFase3.java
+++ Exercici_Lletres/src/Lletres/LletresFase3.java
@@ -2,42 +2,43 @@ package Lletres;
 import java.util.*;
 
 public class LletresFase3 {
-	static ArrayList<Character> nameList = new ArrayList<>(Arrays.asList('A', 'l', 'e', 'j', 'a', 'n', 'd', 'r', 'o'));
+	static List<Character> nameList;
 	
 	public static void main(String[] args) {
+		nameList = initName();
 		printWord(nameList);
-		printCapcelera();
-		printQuantitatLletres(comptaLletres(nameList));		
+		printHeader();
+		printCharacterCount(characterCount(nameList));		
 	}
-	public static void printWord(ArrayList<Character> word) {
+	public static List<Character> initName(){
+		return new ArrayList<>(Arrays.asList('A', 'l', 'e', 'j', 'a', 'n', 'd', 'r', 'o'));
+	}
+	public static void printWord(List<Character> word) {
 		System.out.print("La paraula \"");
 		for(int i = 0; i < word.size(); i++) {
 			System.out.printf("%c", word.get(i));
 		}
 		System.out.printf("\" conté: \n\n");
 	}
-	public static void printCapcelera() {
+	public static void printHeader() {
 		System.out.printf("Lletra\tQuantitat\n");
 		System.out.printf("------\t---------\n");
 	}
-	public static HashMap<Character, Integer> comptaLletres(ArrayList<Character> word) {
-		HashMap<Character, Integer> mapaLletres = new HashMap<>();
+	public static Map<Character, Integer> characterCount(List<Character> word) {
+		Map<Character, Integer> characterMap = new HashMap<>();
 		for(int i = 0; i < word.size(); i++) {
-			if (mapaLletres.containsKey(Character.toUpperCase(word.get(i)))){
-				int j = mapaLletres.get(Character.toUpperCase(word.get(i)));
-				j ++;
-				mapaLletres.put(Character.toUpperCase(word.get(i)), j);
-				
+			if (characterMap.containsKey(Character.toUpperCase(word.get(i)))){
+				characterMap.put(Character.toUpperCase(word.get(i)), characterMap.get(Character.toUpperCase(word.get(i)))+1);
 			}else {
-				mapaLletres.put(Character.toUpperCase(word.get(i)), 1);
+				characterMap.put(Character.toUpperCase(word.get(i)), 1);
 			}
 		}
-		return mapaLletres;
+		return characterMap;
 	}	
 	@SuppressWarnings("rawtypes")
-	public static void printQuantitatLletres(HashMap<Character, Integer> llistaQ) {
-		Set lletres = llistaQ.entrySet();
-		Iterator iter = lletres.iterator();
+	public static void printCharacterCount(Map<Character, Integer> mapChars) {
+		Set set = mapChars.entrySet();
+		Iterator iter = set.iterator();
 		while(iter.hasNext()) {
 			Map.Entry mentry = (Map.Entry)iter.next();
 			System.out.printf("%c\t%d\n", mentry.getKey(), mentry.getValue());
